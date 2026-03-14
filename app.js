d8deb6b8443028e4258aec3540dc2e18bea8b891
Priyan Arora <Priyanarora22@gmail.com>
2026-03-14 17:18:05 -0300

    Pushing exisiting code base already made from local

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+//db connection
+main()
+  .then(() =>{
+    console.log("Connected to DB");
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.listen(8080, () =>{
+  console.log("server is listening to port 8080");
+});
+
+//home page
+app.get("/",  (req,res)=>{
+  res.send("Hi, I am root");
+});
+
+//Index route
+app.get("/listings", async (req,res)=>{
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", {allListings});
+});
+
+//New Route
+app.get("/listings/new", (req, res)=>{
+  res.render("./listings/new.ejs");
+});
+
+//Create route -> new route sends post request through its new.ejs which we will use to create below
+app.post("/listings", async(req, res)=>{
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit route
+app.get("/listings/:id/edit", async (req, res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", {listing});
+});
+
+//Update route
+app.put("/listings/:id", async(req,res)=>{
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  res.redirect(`/listings`);
+});
+
+//Show route (keeping before new route as :id searches for new in database, js is weird man)
+app.get("/listings/:id", async (req,res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/show.ejs", {listing});
+});
+
+//delete route
+app.delete("/listings/:id", async(req,res)=>{
+  let {id} = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
+
+
---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1761839257961-4dce65b72d99?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) => 
+      (v== "") 
+        ? "https://images.unsplash.com/photo-1761839257961-4dce65b72d99?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
